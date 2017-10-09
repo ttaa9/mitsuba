39aafde4ac81ef2e3ab9033fbc1739c4945532b0
L. E. Segovia <amyspark@users.noreply.github.com>
2017-10-09 20:41:05 -0300

    Add travis and appveyor  - Includes fixes for MacOS (avoid using sysroot)  - Includes fixes for Windows (work around VS2017)  - Includes fixes for Linux (pull request submitted; see thread.cpp)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 *DISCLAIMER: This is a **fork** of [the original repository](https://github.com/mitsuba-renderer/mitsuba). Report issues, pull requests, etc. by following the badges below. Thanks!*
 
-# Mitsuba renderer ![][issues] ![][pulls] ![][stars] ![][license] ![][travis] ![][appveyor]
+# Mitsuba renderer [![][issues]][issues-link] [![][pulls]][pulls-link] [![][stars]][stars-link] [![][license]][license-link] [![][travis]][travis-link] [![][appveyor]][appveyor-link]
 
 Mitsuba is a physically-based, research-oriented renderer.
 
@@ -28,8 +28,14 @@ Mitsuba is a physically-based, research-oriented renderer.
 ```
 
 [issues]: https://img.shields.io/github/issues/mitsuba-renderer/mitsuba.svg (GitHub issues)
+[issues-link]: https://github.com/mitsuba-renderer/mitsuba/issues
 [pulls]: https://img.shields.io/github/issues-pr/mitsuba-renderer/mitsuba.svg (GitHub pull requests)
+[pulls-link]: https://github.com/mitsuba-renderer/mitsuba/pulls
 [stars]: https://img.shields.io/github/stars/mitsuba-renderer/mitsuba.svg (GitHub stars)
-[license]: https://img.shields.io/badge/license-GPL3-blue.svg (./LICENSE)
-[travis]: https://img.shields.io/travis/amyspark/mitsuba.svg
-[appveyor]:  https://ci.appveyor.com/api/projects/status/32r7s2skrgm9ubva?svg=true
+[stars-link]: https://github.com/mitsuba-renderer/mitsuba/stargazers
+[license]: https://img.shields.io/badge/license-GPL3-blue.svg (License: GPLv3)
+[license-link]: ./LICENSE
+[travis]: https://img.shields.io/travis/amyspark/mitsuba.svg (Travis-CI status)
+[travis-link]: https://travis-ci.org/amyspark/mitsuba
+[appveyor]: https://ci.appveyor.com/api/projects/status/8v8u6lnc8krqxkuu?svg=true (Appveyor status)
+[appveyor-link]: https://ci.appveyor.com/project/amyspark/mitsuba
---
+++ build/config-macos10.12-clang-x86_64-travis.py
@@ -0,0 +1,32 @@
+BUILDDIR       = '#build/release'
+DISTDIR        = '#Mitsuba.app'
+CXX            = 'clang++'
+CC             = 'clang'
+CCFLAGS        = ['-mmacosx-version-min=10.12', '-march=native', '-funsafe-math-optimizations', '-fno-math-errno', '-O3', '-Wall', '-Wno-deprecated-declarations', '-g', '-DMTS_DEBUG', '-DSINGLE_PRECISION', '-DSPECTRUM_SAMPLES=3', '-DMTS_SSE', '-DMTS_HAS_COHERENT_RT', '-fvisibility=hidden', '-ftemplate-depth=512', '-stdlib=libc++']
+LINKFLAGS      = ['-framework', 'OpenGL', '-framework', 'Cocoa', '-mmacosx-version-min=10.12', '-Wl,-headerpad,128', '-stdlib=libc++']
+CXXFLAGS       = ['-std=c++11']
+BASEINCLUDE    = ['#include', '#dependencies/include']
+BASELIBDIR     = ['#dependencies/lib']
+BASELIB        = ['m', 'pthread', 'Half']
+OEXRINCLUDE    = ['#dependencies/include/OpenEXR']
+OEXRLIB        = ['IlmImf', 'Imath', 'Iex', 'z']
+PNGLIB         = ['png16']
+PNGINCLUDE     = ['#dependencies/include/libpng']
+JPEGLIB        = ['jpeg']
+JPEGINCLUDE    = ['#dependencies/include/libjpeg']
+XERCESLIB      = ['xerces-c']
+GLLIB          = ['GLEWmx', 'objc']
+GLFLAGS        = ['-DGLEW_MX']
+BOOSTINCLUDE   = ['#dependencies']
+BOOSTLIB       = ['boost_filesystem', 'boost_system', 'boost_thread']
+PYTHON27INCLUDE= ['/System/Library/Frameworks/Python.framework/Versions/2.7/Headers']
+PYTHON27LIBDIR = ['/System/Library/Frameworks/Python.framework/Versions/2.7/lib']
+PYTHON27LIB    = ['boost_python27', 'boost_system']
+PYTHON35INCLUDE= ['#dependencies/include/python3.5']
+PYTHON35LIB    = ['boost_python36', 'boost_system']
+PYTHON36INCLUDE= ['#dependencies/include/python3.4']
+PYTHON36LIB    = ['boost_python36', 'boost_system']
+COLLADAINCLUDE = ['/usr/local/include/collada-dom2.4', '/usr/local/include/collada-dom2.4/1.4']
+COLLADALIB     = ['collada-dom2.4-dp']
+QTDIR          = '#dependencies'
+FFTWLIB        = ['fftw3']
